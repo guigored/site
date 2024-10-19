2130e118cd9c4f2c28bae48b5ab45c90035e927e
guigored <guigospace@gmail.com>
2024-10-19 18:06:14 -0300

    rotas e config 1 leva compositores

--- app.py
+++ app.py
@@ -37,6 +37,134 @@ def index():
 def adelzonilton():
     return render_template('Adelzonilton.html')
 
+@app.route('/bimba')
+def bimba():
+    return render_template('bimba.html')
+
+@app.route('/russo')
+def russo():
+    return render_template('russo.html')
+
+@app.route('/nilo')
+def nilo():
+    return render_template('nilo.html')
+
+@app.route('/nilson')
+def nilson():
+    return render_template('nilson.html')
+
+@app.route('/barberin')
+def barberin():
+    return render_template('barberin.html')
+
+@app.route('/gil')
+def gil():
+    return render_template('gil.html')
+
+@app.route('/baes')
+def baes():
+    return render_template('baes.html')
+
+@app.route('/popular')
+def popular():
+    return render_template('popular.html')
+
+@app.route('/sergin')
+def sergin():
+    return render_template('sergin.html')
+
+@app.route('/roxinho')
+def roxinho():
+    return render_template('roxinho.html')
+
+@app.route('/pinga')
+def pinga():
+    return render_template('pinga.html')
+
+@app.route('/rody')
+def rody():
+    return render_template('rody.html')
+
+@app.route('/pqd')
+def pqd():
+    return render_template('pqd.html')
+
+@app.route('/titio')
+def titio():
+    return render_template('titio.html')
+
+@app.route('/bombeiro')
+def bombeiro():
+    return render_template('bombeiro.html')
+
+@app.route('/show')
+def show():
+    return render_template('show.html')
+    
+@app.route('/adelino')
+def adelino():
+    return render_template('adelino.html')
+
+@app.route('/betosembraco')
+def betosembraco():
+    return render_template('betosembraco.html')
+
+@app.route('/claudinho')
+def claudinho():
+    return render_template('claudinho.html')
+
+@app.route('/criolodoido')
+def criolodoido():
+    return render_template('criolodoido.html')
+
+@app.route('/gmartins')
+def gmartins():
+    return render_template('gmartins.html')
+
+@app.route('/joel')
+def joel():
+    return render_template('joel.html')
+
+@app.route('/juarez')
+def juarez():
+    return render_template('juarez.html')
+
+@app.route('/marcosdiniz')
+def marcosdiniz():
+    return render_template('marcosdiniz.html')
+
+@app.route('/luizgrande')
+def luizgrande():
+    return render_template('luizgrande.html')
+
+@app.route('/naval')
+def naval():
+    return render_template('naval.html')
+
+@app.route('/neguinbeijaflor')
+def neguinbeijaflor():
+    return render_template('neguinbeijaflor.html')
+
+@app.route('/botina')
+def botina():
+    return render_template('botina.html')
+
+@app.route('/meriti')
+def meriti():
+    return render_template('meriti.html')
+
+@app.route('/walmirpurific')
+def walmirpurific():
+    return render_template('walmirpurific.html')
+
+@app.route('/ronaldao')
+def ronaldao():
+    return render_template('ronaldao.html')
+
+@app.route('/miltinho')
+def miltinho():
+    return render_template('miltinho.html')
+
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
